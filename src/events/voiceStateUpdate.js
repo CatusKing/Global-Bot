2260412d5f55d9441e8aedffaaebbddcf200c1dc
Thomas <chalkking17@gmail.com>
2024-12-16 22:12:04 -0500

    idk
    literally have no clue

--- src/events/voiceStateUpdate.js
+++ src/events/voiceStateUpdate.js
@@ -4,27 +4,29 @@ module.exports = {
   name: 'voiceStateUpdate',
   async execute(oldState, newState, client) {
     try {
-      if (!newState.channel || newState.member.permissions.has('Administrator')) return;
+      if (!newState.channel || newState.member.id === newState.guild.ownerId) return;
+      
       const db = new JsonDB(new Config("db", true, true, './'));
       let data = await db.getData('/data');
       
       // Make sure the required config is available
-      if (data.config && data.config.videoOnly) {
-        const channel = await client.channels.fetch(data.config.videoOnly);
-        
-        if (!channel.isVoiceBased()) return;
-        
-        // Get the AFK channel for the guild
-        const guildAFKChannel = channel.guild.afkChannel || null;
-        
-        if (newState.channel.id === channel.id) {
-          setTimeout(async () => {
-            if (!newState.member.voice.selfVideo) {
-              await newState.member.voice.setChannel(guildAFKChannel, 'Need to have video!');
-            }
-          }, 10000); // 10-second grace period after joining
-        }
-      }
+      if (!data.config || !data.config.videoOnly || newState.channel.id !== channel.id) return;
+      
+      const channel = await client.channels.fetch(data.config.videoOnly);
+      if (!channel.isVoiceBased()) return;
+      
+      // Get the AFK channel for the guild
+      const guildAFKChannel = channel.guild.afkChannel || null;
+      
+      if (newState.channel.id !== channel.id) return;
+      
+      setTimeout(async () => {
+      
+        if (newState.member.voice.selfVideo) return;
+      
+        await newState.member.voice.setChannel(guildAFKChannel, 'Need to have video!');
+      
+        }, 20 * 1000); // 20-second grace period after joining
     } catch (error) {
       console.error('Error in voiceStateUpdate checks:', error);
     }
